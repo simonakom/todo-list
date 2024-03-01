7515f5a36432a8cd92456822d6b7a6c8ec65858d
simonakomarovska <simonak399@gmail.com>
2024-03-01 16:18:59 +0100

    base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/todo-list",
+  // base: "/todo-list",
+  base: "",
 })
